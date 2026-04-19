62b382b1191cfa0a2f1d51d7ac2630bb32fba6e9
tudorcalu <tudycalu@gmail.com>
2026-04-19 16:40:43 +0300

    main update

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,9 @@ import org.example.pipeline.WorkflowOrchestrator;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Processing video ingest pipeline");
+        VideoFile master = new VideoFile("master_100GB_101.mkv", 100_000_000_000L);
+        WorkflowOrchestrator orchestrator = new WorkflowOrchestrator(master);
+        orchestrator.runPipeline();
     }
 }
