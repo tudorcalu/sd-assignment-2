895b900cd34d259e7eb58f4262f563b8384b45f0
tudorcalu <tudycalu@gmail.com>
2026-04-19 14:10:24 +0300

    content filter for compliance stage

---
+++ src/main/java/org/example/pipeline/ContentFilter.java
@@ -0,0 +1,9 @@
+package org.example.pipeline;
+
+import org.example.models.VideoFile;
+
+public class ContentFilter {
+    public void filterContent(VideoFile videoFile) {
+        System.out.println("Running automated checks for region-specific compliance on " + videoFile.getFilename());
+    }
+}
