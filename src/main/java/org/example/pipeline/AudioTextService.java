71005c6cb72f09ed8743b9e5a31f38a0268b7443
tudorcalu <tudycalu@gmail.com>
2026-04-19 13:58:43 +0300

    audio service and worflow implementation

---
+++ src/main/java/org/example/pipeline/AudioTextService.java
@@ -0,0 +1,23 @@
+package org.example.pipeline;
+
+import org.example.models.VideoFile;
+
+public class AudioTextService {
+    private AudioNormalizer audioNormalizer;
+    private LanguageDetector languageDetector;
+    private SubtitleGenerator subtitleGenerator;
+
+    public AudioTextService() {
+        this.audioNormalizer = new AudioNormalizer();
+        this.languageDetector = new LanguageDetector();
+        this.subtitleGenerator = new SubtitleGenerator();
+    }
+
+    public void process(VideoFile videoFile) {
+        System.out.println("Starting Audio/Text Phase");
+        audioNormalizer.normalize(videoFile);
+        languageDetector.detectLanguage(videoFile);
+        subtitleGenerator.generateSubtitles(videoFile);
+        System.out.println("Audio/Text Phase completed successfully");
+    }
+}
